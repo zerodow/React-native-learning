ef806c10aa13d506eeeb7be8a59c26f16aaf360c
Duc <minhducc96@gmail.com>
2018-09-01 15:41:46 +0700

    1/9

--- Homework/Buoi2/App.js
+++ Homework/Buoi2/App.js
@@ -4,13 +4,6 @@ import { styles } from './styles';
 import Logo from './components/Logo';
 import Schedule from './components/Schedule';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 export default class App extends Component {
   render() {
     return (
